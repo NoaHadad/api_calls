52d9bf505446686b3d7d8203d3a1f6758dc1a1c8
NoaHadad <34161861+NoaHadad@users.noreply.github.com>
2021-03-26 00:22:31 +0200

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject2',
+        'NAME': 'myproject',
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
